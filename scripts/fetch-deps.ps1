eeac3a74e66c5b030d9815befae7c8051ed77f34
Christoph M. Becker <cmbecker69@gmx.de>
2024-10-16 21:04:52 +0200

    Accept any vs version for a dependency from a series file

--- scripts/fetch-deps.ps1
+++ scripts/fetch-deps.ps1
@@ -34,7 +34,7 @@ $needs = @{}
 $response = Invoke-WebRequest "https://downloads.php.net/~windows/php-sdk/deps/series/packages-$version-$vs-$arch-$stability.txt"
 foreach ($line in $response.Content -split "`r?`n") {
     foreach ($dep in $deps) {
-        if ($line -match "$dep-(.+)-$vs-$arch.zip") {
+        if ($line -match "$dep-(.+)-v[cs]\d\d-$arch\.zip") {
             $needs.$dep = $matches[1]
         }
     }
